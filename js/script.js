143163218446124ac34f81c517212805f61a3968
Łukasz Potoczniak <lukaszpotoczniak@gmail.com>
2019-11-14 22:25:47 +0100

    rework game

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+const randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if(randomNumber === 1){
   computerMove = 'kamień'; 
 }else if (randomNumber == 2){
   computerMove = 'papier';
-}else (randomNumber == 3){
+}else (randomNumber === 3){
     computerMove = 'nożyce';
 }
 
-printMessage('Mój ruch to: ' + computerMove);
+console.log('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -20,29 +20,29 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if(playerInput === '1'){
   playerMove = 'kamień';
-}else if(playerInput == '2'){
+}else if(playerInput ==='2'){
   playerMove = 'papier';  
-}else if(playerInput == '3'){
+}else if(playerInput ==='3'){
     playerMove = 'nożyce';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+console.log('Twój ruch to: ' + playerMove);
 
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  }else if(computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-  }else if(computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
+if( computerMove === 'kamień' && playerMove === 'papier'){
+    console.log('Ty wygrywasz!');
+  }else if(computerMove === 'papier' && playerMove === 'nożyce'){
+    console.log('Ty wygrywasz!');
+  }else if(computerMove === 'nożyce' && playerMove === 'kamień'){
+    console.log('Ty wygrywasz!');
   }
 
-if( computerMove == 'kamień' && playerMove == 'kamień') {
-    printMessage('Remis');  
-  } else if(computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis');  
-  } else if(computerMove == 'nożyce' && playerMove == 'nożyce')
-    printMessage('Remis');    
+if( computerMove === 'kamień' && playerMove == 'kamień') {
+    console.log('Remis');  
+  } else if(computerMove === 'papier' && playerMove === 'papier'){
+    console.log('Remis');  
+  } else if(computerMove === 'nożyce' && playerMove === 'nożyce'){
+    console.log('Remis');    
   }
