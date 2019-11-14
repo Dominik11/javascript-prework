7303d25ceb62aaa5b5d84bb1317cdeb5b2702be5
Łukasz Potoczniak <lukaszpotoczniak@gmail.com>
2019-11-14 20:51:48 +0100

    Made a game

--- js/script.js
+++ js/script.js
@@ -1,13 +1,48 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-    document.getElementById('messages').appendChild(div);
-    let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-    let playerMove = 'papier'
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień'; 
+}else if (randomNumber == 2){
+  computerMove = 'papier';
+}else (randomNumber == 3){
+    computerMove = 'nożyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}else if(playerInput == '2'){
+  playerMove = 'papier';  
+}else if(playerInput == '3'){
+    playerMove = 'nożyce';
 }
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+
+if( computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis');  
+  } else if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis');  
+  } else if(computerMove == 'nożyce' && playerMove == 'nożyce')
+    printMessage('Remis');    
+  }
